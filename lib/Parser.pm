7444dd407181c833d41720c6d622b0183bf25187
Jesse Alama <jesse.alama@gmail.com>
2012-04-05 19:34:25 +0100

    Grammar hackage.
    Starting to get tricky.

--- lib/Parser.pm
+++ lib/Parser.pm
@@ -12,42 +12,58 @@ use Readonly;
 
 our @EXPORT_OK = qw(parse);
 
+# $::RD_HINT = 1;
+
 Readonly my $EMPTY_STRING => q{};
 
 my $grammar = join ($EMPTY_STRING, <DATA>);
 
-# carp 'Grammar:', "\N{LF}", Dumper ($grammar);
-
 my $parser = Parse::RecDescent->new ($grammar);
+
+sub delete_tptp_comments {
+    my $text = shift;
+    my @final_lines = ();
+    my @lines = split ("\N{LF}", $text);
+    foreach my $line (@lines) {
+	$line =~ s/ [%].*\z / /;
+	push (@final_lines, $line);
+    }
+    return join ("\N{LF}", @final_lines);
+}
 
 sub parse {
     my $text = shift;
+    $text = delete_tptp_comments ($text);
     return $parser->tptp_file ($text);
 }
 
 1;
 __DATA__
 
-#<autotree>
+{use Data::Dumper;}
 
-# <autoaction: {
-#     # foreach my $k (keys %item) {
-#     # 	if (defined $item{$k}) {
-#     # 	    print $k, ' : ', Dumper ($item{$k});
-#     # 	} else {
-#     # 	    print $k, ' : (undefined)', "\n";
-#     # 	}
-#     # }
-#     [@item];
-# } >
+<autotree>
+
+ # <autoaction: {
+ #      foreach my $k (keys %item) {
+ #      	if (defined $item{$k}) {
+ #      	    print $k, ' : ', Dumper ($item{$k});
+ #      	} else {
+ #      	    print $k, ' : (undefined)', "\n";
+ #      	}
+ #      }
+ #     [@item];
+ # } >
 
 tptp_file: tptp_input(s?) eofile
 
-tptp_input: annotated_formula | include | comment
+tptp_input: annotated_formula | include
 
 annotated_formula: fof_annotated
 
-fof_annotated: fof_token comment(s?) left_paren comment(s?) name comment(s?) comma comment(s?) formula_role comment(s?) comma comment(s?) fof_formula comment(s?) right_paren comment(s?) full_stop | <error>
+fof_annotated:
+    'fof' '(' name ',' formula_role ',' fof_formula ')' '.'
+  | <error>
 
 variable: upper_word
 
@@ -103,7 +119,7 @@ double_quote: /["]/
 
 do_char: /[a-z]/
 
-formula_data: dollar fof_token left_paren fof_formula right_paren
+formula_data: '$' 'fof' '(' fof_formula ')'
 
 atomic_word: lower_word | single_quoted
 
@@ -129,111 +145,97 @@ numeric: /[0-9]/
 
 alpha_numeric: /[a-zA-Z0-9_]/
 
-vline: '|'
+fof_formula:
+    fof_logic_formula
+  | fof_sequent
 
-fof_formula: fof_logic_formula | fof_sequent
-fof_logic_formula: fof_binary_formula | fof_unitary_formula
-fof_binary_formula: fof_binary_nonassoc | fof_binary_assoc
-fof_binary_nonassoc: fof_unitary_formula comment(s?) binary_connective comment(s?) fof_unitary_formula
+fof_logic_formula:
+    fof_binary_formula
+  | fof_unitary_formula
+
+fof_binary_formula:
+    fof_binary_nonassoc
+  | fof_binary_assoc
+
+fof_binary_nonassoc:
+  fof_unitary_formula
+  ('<=>' | '=>' | '<=' | '<~>' | '~|' | '~&')
+  fof_unitary_formula
+
 fof_binary_assoc:
     fof_or_formula
   | fof_and_formula
 
-fof_or_formula: fof_unitary_formula comment(s?) vline comment(s?) fof_unitary_formula
-  | fof_unitary_formula comment(s?) vline comment(s?) fof_or_formula
+fof_or_formula:
+    fof_unitary_formula '|' (fof_or_formula | fof_unitary_formula)
 
 fof_and_formula:
-  fof_unitary_formula conjunction_sign fof_unitary_formula
-  | fof_unitary_formula conjunction_sign fof_and_formula
+    fof_unitary_formula '&' (fof_and_formula | fof_unitary_formula)
 
 fof_unitary_formula:
     fof_quantified_formula
   | fof_unary_formula
   | atomic_formula
-  | left_paren comment(s?) fof_logic_formula comment(s?) right_paren
+  | '(' fof_logic_formula ')'
 
 fof_quantified_formula:
-  fol_quantifier comment(s?)
-  left_square_bracket comment(s?) fof_variable_list comment(s?) right_square_bracket comment(s?)
-  colon comment(s?)
-  fof_unitary_formula
+    fol_quantifier
+    '[' fof_variable_list ']'
+    ':'
+    fof_unitary_formula
 
 fof_variable_list:
-    variable comment(s?) comma comment(s?) fof_variable_list
+    variable ',' <commit> fof_variable_list
   | variable
 
-fof_unary_formula: unary_connective comment(s?) fof_unitary_formula | fol_infix_unary
+fof_unary_formula: unary_connective fof_unitary_formula | fol_infix_unary
 
-fol_infix_unary: term comment(s?) infix_equality comment(s?) term
+fol_infix_unary: term infix_equality term
 
-binary_connective:
-    equivalence_connective
-  | implies_connective
-  | follows_from_connective
-  | disequivalence_connective
-  | not_or_connective vline
-  | not_and_connective
-
-conjunction_sign: /[&]/
-equivalence_connective: '<=>'
-implies_connective: '=>'
-follows_from_connective: '<='
-disequivalence_connective: '<~>'
-not_or_connective: '~|'
-not_and_connective: '~&'
-
-fol_quantifier: /[!?]/
+fol_quantifier: '!' | '?'
 
 atomic_formula:
     plain_atomic_formula
   | defined_atomic_formula
-  | system_atomic_formula
 
 plain_atomic_formula: plain_term
 
 plain_term:
-    functor comment(s?) left_paren comment(s?) arguments comment(s?) right_paren
+    functor '(' <commit> arguments ')'
   | constant
 constant: functor
 functor: atomic_word
 arguments:
-    term comment(s?) comma comment(s?) arguments
+    term ',' arguments
   | term
 term: functor_term | variable
-functor_term: plain_term | defined_term | system_term
+functor_term: plain_term | defined_term
 defined_term: defined_atom | defined_atomic_term
 defined_atom: number | distinct_object
 defined_atomic_term: defined_plain_term
 
 defined_plain_term:
-    defined_constant
-  | defined_functor comment(s?) left_paren comment(s?) arguments comment(s?) right_paren
+    defined_functor '(' <commit> arguments ')'
+  | defined_constant
 
 defined_constant: defined_functor
 defined_functor: atomic_defined_word
 atomic_defined_word: dollar_word
-dollar_word: dollar lower_word
-dollar: '$'
-include:
-  include_keyword comment(s?) left_paren comment(s?) file_name comment(s?) right_paren comment(s?) formula_selection comment(s?) right_paren comment(s?) full_stop
-  | include_keyword comment(s?) left_paren comment(s?) file_name comment(s?) right_paren comment(s?) full_stop
+dollar_word: '$' lower_word
+include: 'include' '(' file_name ')' '.'
 
-system_atomic_formula: system_term
-system_term: system_constant | (system_functor '(' arguments ')' )
-system_functor: atomic_system_word
 atomic_system_word: dollar_dollar_word
-dollar_dollar_word: dollar dollar lower_word
+dollar_dollar_word: '$$' lower_word
 
 defined_atomic_formula: defined_plain_formula | defined_infix_formula
 
-unary_connective: /[~]/
+unary_connective: '~'
 
 name: atomic_word | integer
 infix_equality: '='
 file_name: single_quoted
 formula_selection: ',' '[' name_list ']' | null
 name_list: name | name ',' name_list
-system_constant: system_functor
 defined_plain_formula: defined_plain_term
 defined_plain_term: defined_constant | defined_functor '(' arguments ')'
 defined_infix_formula: term defined_infix_pred term
@@ -248,13 +250,3 @@ eofile: /\Z/
 comment: comment_line
 comment_line: /[%].*/
 printable_char: /./
-
-left_paren: '('
-right_paren: ')'
-comma: ','
-fof_token: 'fof'
-full_stop: '.'
-left_square_bracket: '['
-right_square_bracket: ']'
-colon: /[:]/
-include_keyword: 'include'
