79b58983e7b2540087b6b6650d8aea0e3058a949
Jesse Alama <jesse.alama@gmail.com>
2012-04-19 03:27:25 +0200

    building steam with a grain of salt

---
+++ lib/CategoricalCommand.pm
@@ -0,0 +1,13 @@
+# check whether a theory is categorical in a certain finite power
+
+# a few ways to proceed:
+#
+# - given a theory and cardinality n, output a new TPTP file
+# representing the problem of showing that the theory is categorical
+#
+# - given a theory and a model of it, output a new TPTP file
+# representing the problem of showing that any model of the theory is
+# isomorphic to the given one
+
+# not only construct the problem, but try to solve it.  (I guess it is
+# a graph isomorphism problem.)
