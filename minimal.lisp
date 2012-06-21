2f03444ce8ff71e7733ea1b6a6bfd5ba89346460
Jesse Alama <jesse.alama@gmail.com>
2012-06-21 13:48:08 +0200

    Cleaning.

--- minimal.lisp
+++ minimal.lisp
@@ -30,58 +30,48 @@
   (declare (ignore keep))
   (if skip-initial-proof
       (call-next-method)
-      (let ((initial-result (solve *eprover* problem)))
-	(let ((status (szs-status initial-result)))
-	  (if (szs-implies? status (lookup-szs-status "Theorem"))
-	      (let ((result-as-db (interpret initial-result)))
-		(let ((used-premises (used-premises result-as-db problem)))
-		  (let ((trimmed-problem (make-instance 'derivability-problem
-							:conjecture (conjecture problem)
-							:formulas (mapcar #'(lambda (name) (formula-with-name problem name)) used-premises))))
-		    (let ((needed (needed-premises trimmed-problem)))
-		      (minimize trimmed-problem
-				:skip-initial-proof t
-				:keep needed)))))
-	      (error "The initial problem could not be solved (SZS status ~a)." status))))))
+      (let* ((initial-result (solve *eprover* problem))
+	     (status (szs-status initial-result)))
+	(if (szs-implies? status (lookup-szs-status "Theorem"))
+	    (let* ((conjecture (conjecture problem))
+		   (result-as-db (interpret initial-result))
+		   (used-premise-names (used-premises result-as-db problem))
+		   (used-premises (mapcar #'(lambda (name) (formula-with-name problem name)) used-premise-names))
+		   (trimmed-problem (make-derivability-problem (cons conjecture used-premises)))
+		   (needed (needed-premises trimmed-problem)))
+	      (minimize trimmed-problem
+			:skip-initial-proof t
+			:keep needed))
+	    (error "The initial problem could not be solved (SZS status ~a)." status)))))
 
 (defmethod minimize ((problem derivability-problem) &key skip-initial-proof keep)
   (declare (ignore skip-initial-proof))
-  (let ((premises (premises problem))
-	(conjecture (conjecture problem))
+  (let ((conjecture (conjecture problem))
+	(premises (formulas problem))
 	(solutions nil)
 	(unknown nil)
 	(non-solutions nil))
-    (labels ((known-solution (solution)
-	       (some #'(lambda (other-subset) (subsetp other-subset solution))
-			   solutions))
-	     (test-subset (subset)
-	       (unless (known-solution subset)
-		 (when (subsetp keep subset)
-		   (let ((smaller-problem (make-instance 'derivability-problem
-							 :conjecture conjecture
-							 :formulas subset)))
-		     (let ((szs-status (solve (list *eprover* *paradox*) smaller-problem)))
-		       (if (is-szs-success? szs-status)
-			   (if (szs-implies? szs-status
-					     (lookup-szs-status "Theorem"))
-			       (push subset solutions)
-			       (push subset non-solutions))
-			   (push subset unknown))))))))
-      (loop
-	 for n from 0 upto (length premises)
-	 do
-	   (map-combinations #'test-subset premises :length n)
-	 finally
-	   (return (values solutions non-solutions unknown))))))
+    (flet ((test-subset (subset)
+	     (when (and (subsetp keep subset) (not (some-subset subset solutions)))
+	       (let* ((p (make-derivability-problem (cons conjecture subset)))
+		      (szs-status (solve (list *eprover* *paradox*) p)))
+		 (setf subset (mapcar #'name subset))
+		 (setf subset (sort subset #'string<))
+		 (if (is-szs-success? szs-status)
+		     (if (szs-implies? szs-status (lookup-szs-status "Theorem"))
+			 (push subset solutions)
+			 (push subset non-solutions))
+		     (push subset unknown))))))
+      (map-all-combinations #'test-subset premises))
+    (values solutions non-solutions unknown)))
 
 (defmethod minimize ((problem pathname) &key skip-initial-proof keep)
   (let ((db (read-tptp problem)))
     (let ((conjecture (conjecture-formula db)))
       (if conjecture
-	  (let ((problem (make-instance 'derivability-problem
-					:conjecture conjecture
-					:formulas (non-conjecture-formulas db))))
-	    (minimize problem
+	  (let ((p (make-derivability-problem (cons conjecture
+						    (non-conjecture-formulas db)))))
+	    (minimize p
 		      :skip-initial-proof skip-initial-proof
 		      :keep keep))
 	  (minimize db
