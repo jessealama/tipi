f6952f0ae5df340df4581c1900e9e938719137d1
Jesse Alama <jesse.alama@gmail.com>
2012-10-11 15:48:59 +0200

    Non-functioning parser.

--- parse.lisp
+++ parse.lisp
@@ -41,26 +41,42 @@
            (maybe-unread c stream)
            (when (null v)
              (lexer-error c))
-           (return-from read-id (intern-id (coerce (nreverse v) 'string))))
+           (return-from read-id (make-symbol (coerce (nreverse v) 'string))))
+         (push c v)))))
+
+(defun read-word (stream)
+  (let ((v '()))
+    (loop
+       (let ((c (read-char stream nil nil)))
+         (when (or (null c)
+                   (not (or (digit-char-p c) (alpha-char-p c) (eql c #\_))))
+           (maybe-unread c stream)
+           (when (null v)
+             (lexer-error c))
+           (return-from read-word (make-symbol (coerce (nreverse v) 'string))))
          (push c v)))))
 
 (defun lexer (&optional (stream *standard-input*))
   (loop
      (let ((c (read-char stream nil nil)))
        (cond
-         ((member c '(#\Space #\Tab)))
-         ((member c '(nil #\Newline)) (return-from lexer (values nil nil)))
-         ((member c '(#\+ #\- #\* #\/ #\( #\)))
+	 ((member c '(#\Space #\Tab)))
+	 ((member c '(nil #\Newline)) (return-from lexer (values nil nil)))
+
+	 ((member c '(#\( #\) #\. #\' #\[ #\] #\: #\! #\? #\,))
+	  (break "Got a symbol: ~a" c)
           (let ((symbol (intern (string c) '#.*package*)))
             (return-from lexer (values symbol symbol))))
-         ((digit-char-p c)
-          (unread-char c stream)
-          (return-from lexer (values 'int (read-number stream))))
-         ((alpha-char-p c)
-          (unread-char c stream)
-          (return-from lexer (values 'id (read-id stream))))
-         (t
-          (lexer-error c))))))
+
+	 ;; try to read an atom
+	 ((alpha-char-p c)
+	  (unread-char c stream)
+	  (let ((next-word (read-word stream)))
+	    (break "next-word = ~a" next-word)
+	    (return-from lexer (values (intern (symbol-name next-word)) next-word))))
+
+	 (t
+	  (lexer-error c))))))
 
 ;;; The parser
 
@@ -77,11 +93,86 @@
 
 (define-parser *tptp-v5.4.0.0-parser*
   (:start-symbol tptp-file)
-  (:terminals (atom integer))
+  (:terminals (|fof|
+	       |(|
+	       |)|
+	       |.|
+	       |,|
+	       |[| |]|
+	       |a| |b| |c| |d| |e| |f| |g| |h| |i| |j| |k| |l| |m|
+	       |n| |o| |p| |q| |r| |s| |t| |u| |v| |w| |x| |y| |z|
+	       |A| |B| |C| |D| |E| |F| |G| |H| |I| |J| |K| |L| |M|
+	       |N| |O| |P| |Q| |R| |S| |T| |U| |V| |W| |X| |Y| |Z|
+	       |0| |1| |2| |3| |4| |5| |6| |7| |8| |9|
+	       |_|
+	       |'|
+	       |axiom|
+	       |hypothesis|
+	       |definition|
+	       |assumption|
+	       |lemma|
+	       |theorem|
+	       |conjecture|
+	       |negated_conjecture|
+	       |plain|
+	       |fi_domain|
+	       |fi_functors|
+	       |fi_predicates|
+	       |type|
+	       |unknown|
+	       |<=>|
+	       |=>|
+	       |<=|
+	       |<~>|
+	       ;; |~|
+	       |~&|
+	       |:|
+	       |!|
+	       |?|
+	       |~|
+	       |+|
+	       |-|
+	       |/|
+	       |E|
+	       |e|
+	       |!=|
+	       |$oType|
+	       |$o|
+	       |$iType|
+	       |$tType|
+	       |$real|
+	       |$rat|
+	       |$int|
+
+	       |$uminus|
+	       |$sum|
+	       |$difference|
+	       |$product|
+	       |$quotient|
+	       |$quotient_e|
+	       |$quotient_t|
+	       |$quotient_f|
+	       |$remainder_e|
+	       |$remainder_t|
+	       |$remainder_f|
+	       |$floor|
+	       |$ceiling|
+	       |$truncate|
+	       |$round|
+	       |$to_int|
+	       |$to_rat|
+	       |$to_real|
+	       |$$|
+	       |&|
+	       |\||
+	       ))
 
   (tptp-file
+   tptp-inputs)
+
+  (tptp-inputs
    ()
-   tptp-input)
+   (tptp-input tptp-inputs))
 
   (tptp-input
    annotated-formula
@@ -91,27 +182,26 @@
    fof-annotated)
 
   (fof-annotated
-   ("fof" "(" name "," formula-role "," fof-formula ")" "."))
+   (|fof| |(| name |,| formula-role |,| fof-formula |)| |.|))
 
   (name
    atomic-word
    integer)
 
   (atomic-word
-   lower-word
-   single-quoted)
+   lower-word)
 
   (lower-word
    lower-alpha
    (lower-alpha alpha-numerics))
 
   (lower-alpha
-   "a" "b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m"
-   "n" "o" "p" "q" "r" "s" "t" "u" "v" "w" "x" "y" "z")
+   |a| |b| |c| |d| |e| |f| |g| |h| |i| |j| |k| |l| |m|
+   |n| |o| |p| |q| |r| |s| |t| |u| |v| |w| |x| |y| |z|)
 
   (upper-alpha
-   "A" "B" "C" "D" "E" "F" "G" "H" "I" "J" "K" "L" "M"
-   "N" "O" "P" "Q" "R" "S" "T" "U" "V" "W" "X" "Y" "Z")
+   |A| |B| |C| |D| |E| |F| |G| |H| |I| |J| |K| |L| |M|
+   |N| |O| |P| |Q| |R| |S| |T| |U| |V| |W| |X| |Y| |Z|)
 
   (alpha-numerics
    alpha-numeric
@@ -121,39 +211,26 @@
    lower-alpha
    upper-alpha
    numeric
-   "_")
+   |_|)
 
   (numeric
-   "0" "1" "2" "3" "4" "5" "6" "7" "8" "9")
-
-  (single-quoted
-   (single-quote sq-char sq-chars single-quote))
-
-  (single-quote
-   "'")
-
-  (sq-char
-   "blah")
-
-  (sq-chars
-   ()
-   (sq-char sq-chars))
+   |0| |1| |2| |3| |4| |5| |6| |7| |8| |9|)
 
   (formula-role
-   "axiom"
-   "hypothesis"
-   "definition"
-   "assumption"
-   "lemma"
-   "theorem"
-   "conjecture"
-   "negated_conjecture"
-   "plain"
-   "fi_domain"
-   "fi_functors"
-   "fi_predicates"
-   "type"
-   "unknown")
+   |axiom|
+   |hypothesis|
+   |definition|
+   |assumption|
+   |lemma|
+   |theorem|
+   |conjecture|
+   |negated_conjecture|
+   |plain|
+   |fi_domain|
+   |fi_functors|
+   |fi_predicates|
+   |type|
+   |unknown|)
 
   (fof-formula
    fof-logic-formula
@@ -175,35 +252,35 @@
    fof-and-formula)
 
   (fof-or-formula
-   (fof-unitary-formula "|" fof-unitary-formula))
+   (fof-unitary-formula |\|| fof-unitary-formula))
 
   (fof-and-formula
-   (fof-unitary-formula "&" fof-unitary-formula))
+   (fof-unitary-formula |&| fof-unitary-formula))
 
   (fof-binary-connective
-   "<=>"
-   "=>"
-   "<="
-   "<~>"
-   "~|"
-   "~&")
+   |<=>|
+   |=>|
+   |<=|
+   |<~>|
+   ;; |~\||
+   |~&|)
 
   (fof-unitary-formula
    fof-quantified-formula
    fof-unary-formula
    atomic-formula
-   ("(" fof-logic-formula ")"))
+   (|(| fof-logic-formula |)|))
 
   (fof-quantified-formula
-   (fol-quantifier "[" fof-variable-list "]" ":" fof-unitary-formula))
+   (fol-quantifier |[| fof-variable-list |]| |:| fof-unitary-formula))
 
   (fol-quantifier
-   "!"
-   "?")
+   |!|
+   |?|)
 
   (fof-variable-list
    variable
-   (variable "," fof-variable-list))
+   (variable |,| fof-variable-list))
 
   (variable
    upper-word)
@@ -216,7 +293,7 @@
    fol-infix-unary)
 
   (unary-connective
-   "~")
+   |~|)
 
   (fol-infix-unary
    (term infix-inequality term))
@@ -224,8 +301,9 @@
   (term
    function-term
    variable
-   conditional-term
-   let-term)
+   ;; conditional-term
+   ;; let-term
+   )
 
   (function-term
    plain-term
@@ -234,7 +312,7 @@
 
   (plain-term
    constant
-   (functor "(" arguments ")"))
+   (functor |(| arguments |)|))
 
   (constant
    functor)
@@ -244,7 +322,7 @@
 
   (arguments
    term
-   (term "," arguments))
+   (term |,| arguments))
 
   (defined-term
       defined-atom
@@ -252,7 +330,8 @@
 
   (defined-atom
       number
-      distinct-object)
+      ;; distinct-object
+      )
 
   (number
    integer
@@ -267,8 +346,8 @@
    (sign unsigned-integer))
 
   (sign
-   "+"
-   "-")
+   |+|
+   |-|)
 
   (unsigned-integer
    decimal)
@@ -278,17 +357,17 @@
    positive-decimal)
 
   (zero-numeric
-   "0")
+   |0|)
 
   (positive-decimal
    (non-zero-numeric numerics))
 
   (numerics
-   ""
+   ()
    (numeric numerics))
 
   (non-zero-numeric
-   "1" "2" "3" "4" "5" "6" "7" "8" "9")
+   |1| |2| |3| |4| |5| |6| |7| |8| |9|)
 
   (rational
    signed-rational
@@ -301,7 +380,7 @@
    (decimal slash positive-decimal))
 
   (slash
-   "/")
+   |/|)
 
   (real
    signed-real
@@ -318,42 +397,42 @@
    (decimal dot-decimal))
 
   (dot-decimal
-   ("." numeric numerics))
+   (|.| numeric numerics))
 
   (decimal-exponent
    (decimal exponent integer)
    (decimal-fraction exponent integer))
 
   (exponent
-   "E"
-   "e")
+   |E|
+   |e|)
 
-  (distinct-object
-   "\"" "distinct" "\"")
+  ;; (distinct-object
+  ;;  "\"" "distinct" "\"")
 
   (defined-atomic-term
       defined-plain-term)
 
   (defined-plain-term
       defined-constant
-      (defined-functor "(" arguments ")"))
+      (defined-functor |(| arguments |)|))
 
   (defined-constant
       defined-functor
       defined-type)
 
   (defined-type
-      "$oType"
-      "$o"
-    "$iType"
-    "$tType"
-    "$real"
-    "$rat"
-    "$int")
+      |$oType|
+      |$o|
+    |$iType|
+    |$tType|
+    |$real|
+    |$rat|
+    |$int|)
 
   (system-term
    system-constant
-   (system-functor "(" arguments ")"))
+   (system-functor |(| arguments |)|))
 
   (system-constant
    system-functor)
@@ -365,37 +444,37 @@
    dollar-dollar-word)
 
   (dollar-dollar-word
-   ("$$" lower-word))
+   (|$$| lower-word))
 
-  (conditional-term
-   "$ite_t(" tff-logic-formula "," term "," term ")")
+  ;; (conditional-term
+  ;;  "$ite_t(" tff-logic-formula "," term "," term ")")
 
-  (let-term
-   ("$let_ft(" tff-let-formula-defn "," term ")")
-   ("$let_ft(" tff-let-term-defn "," term ")"))
+  ;; (let-term
+  ;;  ("$let_ft(" tff-let-formula-defn "," term ")")
+  ;;  ("$let_ft(" tff-let-term-defn "," term ")"))
 
   (infix-inequality
-   "!=")
+   |!=|)
 
   (defined-functor
-      "$uminus"
-      "$sum"
-    "$difference"
-    "$product"
-    "$quotient"
-    "$quotient_e"
-    "$quotient_t"
-    "$quotient_f"
-    "$remainder_e"
-    "$remainder_t"
-    "$remainder_f"
-    "$floor"
-    "$ceiling"
-    "$truncate"
-    "$round"
-    "$to_int"
-    "$to_rat"
-    "$to_real")
+      |$uminus|
+      |$sum|
+    |$difference|
+    |$product|
+    |$quotient|
+    |$quotient_e|
+    |$quotient_t|
+    |$quotient_f|
+    |$remainder_e|
+    |$remainder_t|
+    |$remainder_f|
+    |$floor|
+    |$ceiling|
+    |$truncate|
+    |$round|
+    |$to_int|
+    |$to_rat|
+    |$to_real|)
 
 )
 
@@ -408,20 +487,6 @@
              (format stream "Couldn't evaluate expression ~S"
                      (evaluator-error-expression e)))))
 
-(defun evaluate (e)
-  (labels ((fail () (error (make-condition 'evaluator-error :expression e))))
-    (cond
-      ((numberp e) e)
-      ((eql 'pi e) (int-or-float pi))
-      ((eql 'e e) (int-or-float (exp 1)))
-      ((atom e) (fail))
-      ((= 2 (length e))
-       (unless (eql '- (car e)) (fail))
-       (- (evaluate (cadr e))))
-      ((= 3 (length e))
-       (funcall (binop (car e)) (evaluate (cadr e)) (evaluate (caddr e))))
-      (t (fail)))))
-
 ;;; The toplevel loop
 
 (defun calculator ()
@@ -429,7 +494,7 @@
   (loop
      (with-simple-restart (abort "Return to calculator toplevel.")
        (format t "? ")
-       (let ((e (parse-with-lexer #'lexer *tptp-parser*)))
+       (let ((e (parse-with-lexer #'lexer *tptp-v5.4.0.0-parser*)))
          (when (null e)
            (return-from calculator))
-         (format t " => OK~%")))))
+         (format t " => ~a~%" e)))))
