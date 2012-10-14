eee1e579a4079932fe5eef56e70a48c3059a5682
Jesse Alama <jesse.alama@gmail.com>
2012-10-14 05:04:21 +0200

    Factor out SAME-SYMBOL-OR-NULL.

--- tests.lisp
+++ tests.lisp
@@ -13,30 +13,31 @@
 	    (setf (gethash name symbol-table) new-symbol)
 	    new-symbol)))))
 
+(defun same-symbol-or-null (x y)
+  (cond ((null x)
+	 (null y))
+	((symbolp x)
+	 (and (symbolp y)
+	      (string= (symbol-name x) (symbol-name y))))
+	(t
+	 nil)))
+
 (5am:test lex-tptp
-  (flet ((same-symbol-or-null (x y)
-	   (cond ((null x)
-		  (null y))
-		 ((symbolp x)
-		  (and (symbolp y)
-		       (string= (symbol-name x) (symbol-name y))))
-		 (t
-		  nil))))
-    (let ((target-first-lex-result (list (maybe-make-symbol "fof")
-					 (maybe-make-symbol "(")
-					 (maybe-make-symbol "lower-word")
-					 (maybe-make-symbol ",")
-					 (maybe-make-symbol "axiom")
-					 (maybe-make-symbol ",")
-					 (maybe-make-symbol "lower-word")
-					 (maybe-make-symbol ")")
-					 (maybe-make-symbol ".")
-					 nil))
-	  (lexed (lex-tptp-formula *tptp-formula-1*)))
-      (5am:is (not (null lexed)))
-      (5am:is (starts-with-subseq lexed target-first-lex-result
-				  :test #'same-symbol-or-null))
-      (5am:is (starts-with-subseq target-first-lex-result lexed
-				  :test #'same-symbol-or-null)))))
+  (let ((target-first-lex-result (list (maybe-make-symbol "fof")
+				       (maybe-make-symbol "(")
+				       (maybe-make-symbol "lower-word")
+				       (maybe-make-symbol ",")
+				       (maybe-make-symbol "axiom")
+				       (maybe-make-symbol ",")
+				       (maybe-make-symbol "lower-word")
+				       (maybe-make-symbol ")")
+				       (maybe-make-symbol ".")
+				       nil))
+	(lexed (lex-tptp-formula *tptp-formula-1*)))
+    (5am:is (not (null lexed)))
+    (5am:is (starts-with-subseq lexed target-first-lex-result
+				:test #'same-symbol-or-null))
+    (5am:is (starts-with-subseq target-first-lex-result lexed
+				:test #'same-symbol-or-null))))
 
 ;;; tests.lisp ends here
