e19e9471e4ab16f0d23547d66a58355d2c2c80db
Jesse Alama <jesse.alama@gmail.com>
2013-01-09 02:24:36 +0100

    TSTP solutions and first-order signatures.

---
+++ signature.lisp
@@ -0,0 +1,146 @@
+
+(in-package :tipi)
+
+(defclass first-order-signature ()
+  ((functions
+    :type list
+    :initform nil
+    :initarg :functions
+    :accessor functions)
+   (predicates
+    :type list
+    :initform nil
+    :initarg :predicates
+    :accessor predicates)))
+
+(defparameter *empty-sigature*
+  (make-instance 'first-order-signature))
+
+(defclass function-symbol ()
+  ((arity
+    :type integer
+    :accessor arity
+    :initform (error "An arity is required to create a function symbol.")
+    :initarg :arity)
+   (name
+    :type symbol
+    :accessor name
+    :initform (error "A name is required to create a function symbol.")
+    :initarg :name)))
+
+(defmethod print-object ((object function-symbol) stream)
+  (let ((name (name object))
+	(arity (arity object)))
+    (print-unreadable-object (object stream :type t :identity nil)
+      (format stream "~a (arity ~d)" name arity))))
+
+(defclass predicate-symbol ()
+  ((arity
+    :type integer
+    :accessor arity
+    :initform (error "An arity is required to create a predicate symbol.")
+    :initarg :arity)
+   (name
+    :type symbol
+    :accessor name
+    :initform (error "A name is required to create a predicate symbol.")
+    :initarg :name)))
+
+(defmethod print-object ((object predicate-symbol) stream)
+  (let ((name (name object))
+	(arity (arity object)))
+    (print-unreadable-object (object stream :type t :identity nil)
+      (format stream "~a (arity ~d)" name arity))))
+
+(defmethod print-object ((object first-order-signature) stream)
+  (let ((preds (predicates object))
+	(funcs (functions object)))
+    (print-unreadable-object (object stream :type t :identity nil)
+      (cond ((and (null preds)
+		  (null funcs))
+	     (format stream "(empty)"))
+	    ((null preds)
+	     (format stream "0 predicate symbols; ~d function symbol(s):~%~{~a~%~}" (length funcs) funcs))
+	    ((null funcs)
+	     (format stream "0 function symbols; ~d predicate symbol(s):~%~{~a~%~}" (length preds) preds))
+	    (t
+	     (format stream "~d function symbol(s):~%~{~a~%~}~%~d predicate symbols:~%~{~a~%~}" (length funcs) funcs (length preds) preds))))))
+
+(defun merge-signatures (signature-1 signature-2)
+  (let ((preds-1 (predicates signature-1))
+	(preds-2 (predicates signature-2))
+	(funcs-1 (functions signature-1))
+	(funcs-2 (functions signature-2)))
+    (let ((preds (remove-duplicates (append preds-1 preds-2)
+				    :test #'equal-predicate-symbol-p))
+	  (funcs (remove-duplicates (append funcs-1 funcs-2)
+				    :test #'equal-function-symbol-p)))
+      (make-instance 'first-order-signature
+		     :predicates preds
+		     :functions funcs))))
+
+(defun equal-predicate-symbol-p (pred-symbol-1 pred-symbol-2)
+  (and (string= (name pred-symbol-1)
+		(name pred-symbol-2))
+       (= (arity pred-symbol-1)
+	  (arity pred-symbol-2))))
+
+(defun equal-function-symbol-p (func-symbol-1 func-symbol-2)
+  (and (string= (name func-symbol-1)
+		(name func-symbol-2))
+       (= (arity func-symbol-1)
+	  (arity func-symbol-2))))
+
+(defgeneric signature (tptp)
+  (:documentation "The signature of TPTP."))
+
+(defmethod signature ((tptp tptp-db))
+  (reduce #'merge-signatures (mapcar #'signature
+				     (mapcar #'formula (formulas tptp)))))
+
+(defmethod signature ((formula tptp-formula))
+  (signature (formula formula)))
+
+(defmethod signature ((formula atomic-formula))
+  (let ((arguments (arguments formula)))
+    (let ((pred-symbol (make-instance 'predicate-symbol
+				      :name (predicate formula)
+				      :arity (length arguments))))
+      (let ((sig (make-instance 'first-order-signature
+				:predicates (list pred-symbol))))
+	(reduce #'merge-signatures
+	      (cons sig (mapcar #'signature arguments)))))))
+
+(defmethod signature ((formula unary-connective-formula))
+  (signature (argument formula)))
+
+(defmethod signature ((formula binary-connective-formula))
+  (merge-signatures (signature (lhs formula))
+		    (signature (rhs formula))))
+
+(defmethod signature ((formula generalization))
+  (signature (matrix formula)))
+
+(defmethod signature ((term variable-term))
+  *empty-sigature*)
+
+(defmethod signature ((term function-term))
+  (let ((arguments (arguments term)))
+    (let ((func-symbol (make-instance 'function-symbol
+				      :name (function-symbol term)
+				      :arity (length arguments))))
+      (let ((sig (make-instance 'first-order-signature
+				:functions (list func-symbol))))
+	(reduce #'merge-signatures
+	      (cons sig (mapcar #'signature arguments)))))))
+
+(defun subsignature-p (signature-1 signature-2)
+  (let ((preds-1 (predicates signature-1))
+	(funcs-1 (functions signature-1))
+	(preds-2 (predicates signature-2))
+	(funcs-2 (functions signature-2)))
+    (and (subsetp preds-1 preds-2 :test #'equal-predicate-symbol-p)
+	 (subsetp funcs-1 funcs-2 :test #'equal-function-symbol-p))))
+
+(defun belongs-to-signature (formula signature)
+  (subsignature-p (signature formula) signature))
---
+++ tstp.lisp
@@ -0,0 +1,62 @@
+
+(in-package :tipi)
+
+(defclass tstp-db (tptp-db)
+  ((problem
+    :type tptp-db
+    :accessor problem
+    :initarg :problem)))
+
+(defgeneric read-tstp (tptp-thing source-problem))
+
+(defmethod read-tstp :around ((tptp-thing pathname) source-problem)
+  (if (probe-file tptp-thing)
+      (call-next-method)
+      (error "There is no file at '~a'." (namestring tptp-thing))))
+
+(defmethod read-tstp ((tptp-file pathname) source-problem)
+  (let ((lisp-string (apply-stylesheet *tptp-to-lisp-stylesheet*
+				       (xmlize-tptp tptp-file)
+				       nil
+				       nil))
+	(solution (make-instance 'tstp-db
+				:path tptp-file)))
+    (with-readtable (find-readtable :modern)
+      (let ((tptp-form (handler-case (read-from-string lisp-string)
+			 (error (c) (error "Unable to make sense of~%~%~a~%~%as a Lisp representation of~%~%  ~a~%~%The error was:~%~%  ~a" lisp-string (namestring tptp-file) c)))))
+	(setf (formulas solution) (mapcar #'make-tptp-formula tptp-form))))
+    (setf (problem solution) (read-tptp source-problem))
+    solution))
+
+(defmethod read-tstp ((tptp-string string) source-problem)
+  (let ((temp (temporary-file)))
+    (with-open-file (tptp-file temp
+			       :direction :output
+			       :if-does-not-exist :create
+			       :if-exists :supersede)
+      (format tptp-file "~a" tptp-string))
+    (read-tstp temp source-problem)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Filtering solutions
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defgeneric restrict-solution-to-problem-language (tstp)
+  (:documentation "Restrict TSTP to the language employed by its underlying problem.  The main application is to eexclude Skolem functions and splitting predicates that are present in the solution but not in the problem."))
+
+(defmethod restrict-solution-to-problem-language ((tstp string))
+  (restrict-solution-to-problem-language (read-tstp tstp)))
+
+(defmethod restrict-solution-to-problem-language ((tstp pathname))
+  (restrict-solution-to-problem-language (read-tstp tstp)))
+
+(defmethod restrict-solution-to-problem-language ((tstp tstp-db))
+  (loop
+     with problem = (problem tstp)
+     with problem-signature = (signature problem)
+     for formula in (formulas tstp)
+     when (belongs-to-signature formula problem-signature)
+       collect formula into filtered-formulas
+     finally
+       (return (make-instance 'tptp-db
+			      :formulas filtered-formulas))))
