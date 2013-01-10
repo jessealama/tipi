1b68d948cf4e3d0bc34acd2238da358de0b4acdb
Jesse Alama <jesse.alama@gmail.com>
2013-01-10 04:07:02 +0100

    The subsolution terminating at a given step.
    Essentially just a graph traversal problem.

--- tstp.lisp
+++ tstp.lisp
@@ -54,3 +54,47 @@
      finally
        (return (make-instance 'tptp-db
 			      :formulas filtered-formulas))))
+
+
+(defgeneric subproof-terminating-at (tstp step)
+  (:documentation "The subproof of TSTP that terminates at STEP."))
+
+(defmethod subproof-terminating-at (tstp (step integer))
+  (subproof-terminating-at tstp (format nil "~d" step)))
+
+(defmethod subproof-terminating-at :around ((tstp tstp-db) (step string))
+  (if (formula-with-name tstp step)
+      (call-next-method)
+      (error "No such formula '~a' in the given TSTP database." step)))
+
+(defmethod subproof-terminating-at ((tstp tstp-db) (step string))
+  (let ((formulas (formulas tstp))
+	(q (make-instance 'q))
+	(supporting-formula-table (make-hash-table :test #'equal)))
+    (enqueue-at-front q (list (formula-with-name tstp step)))
+    (loop
+       :until (empty-queue? q)
+       :do
+       (let ((formula (remove-front q)))
+	 (let ((formula-name (name formula)))
+	   (unless (gethash formula-name supporting-formula-table)
+	     (when (slot-boundp formula 'source)
+	       (let ((source (source formula)))
+		 (let ((atoms (flatten source)))
+		   (loop
+		      :for atom in atoms
+		      :for atom-string = (format nil "~a" atom)
+		      :when (formula-with-name tstp atom-string)
+		      :do
+		      (enqueue-at-end q (list (formula-with-name tstp atom-string))))))))
+	   (setf (gethash formula-name supporting-formula-table) t))))
+    (let ((supporting-formulas (hash-table-keys supporting-formula-table)))
+      (let ((sorted-supporting (sort supporting-formulas
+				     #'(lambda (formula-1 formula-2)
+					 (< (position formula-1 formulas :key #'name :test #'string=)
+					    (position formula-2 formulas :key #'name :test #'string=))))))
+	(make-instance 'tstp-db
+		       :formulas (mapcar #'(lambda (name)
+					     (formula-with-name tstp name))
+					 sorted-supporting)
+		       :problem (problem tstp))))))
