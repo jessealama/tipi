d9a1af62e180b70cf2dca1f830f3091f5c6b2b64
Jesse Alama <jesse.alama@gmail.com>
2013-03-04 20:45:19 +0100

    We don't need the BLOCK.

--- solve.lisp
+++ solve.lisp
@@ -30,31 +30,30 @@
    :name "The E theorem prover"
    :solve-function
    (lambda (problem timeout)
-     (block eprover
-       (let* ((path (native-namestring (path problem)))
-	      (eprover-out (make-string-output-stream))
-	      (eprover-err (make-string-output-stream))
-              (eprover-process (run-program "eproof"
-					    (list "--auto"
-						  (format nil "--cpu-limit=~d" timeout)
-						  "--tstp-format"
-						  path)
-					    :search t
-					    :input nil
-					    :output eprover-out
-					    :error eprover-err
-					    :wait t))
-              (eprover-exit-code (process-exit-code eprover-process)))
-         (unwind-protect
-	      (if (zerop eprover-exit-code)
-		  (make-instance 'eprover-result
-				 :text (get-output-stream-string eprover-out))
-		  (make-instance 'eprover-result
-				 :text ""
-				 :szs-status (lookup-szs-status
-					      (if (= eprover-exit-code 6) "Timeout" "Error"))))
-	   (close eprover-out)
-	   (close eprover-err)))))))
+     (let* ((path (native-namestring (path problem)))
+	    (eprover-out (make-string-output-stream))
+	    (eprover-err (make-string-output-stream))
+	    (eprover-process (run-program "eproof"
+					  (list "--auto"
+						(format nil "--cpu-limit=~d" timeout)
+						"--tstp-format"
+						path)
+					  :search t
+					  :input nil
+					  :output eprover-out
+					  :error eprover-err
+					  :wait t))
+	    (eprover-exit-code (process-exit-code eprover-process)))
+       (unwind-protect
+	    (if (zerop eprover-exit-code)
+		(make-instance 'eprover-result
+			       :text (get-output-stream-string eprover-out))
+		(make-instance 'eprover-result
+			       :text ""
+			       :szs-status (lookup-szs-status
+					    (if (= eprover-exit-code 6) "Timeout" "Error"))))
+	 (close eprover-out)
+	 (close eprover-err))))))
 
 (defparameter *paradox*
   (make-instance
@@ -62,30 +61,29 @@
    :name "Paradox"
    :solve-function
    (lambda (problem timeout)
-     (block paradox
-       (let* ((path (native-namestring (path problem)))
-	      (paradox-out (make-string-output-stream))
-	      (paradox-err (make-string-output-stream))
-              (paradox-process (run-program "paradox"
-                                            (list "--tstp"
-                                                  "--no-progress"
-                                                  "--time" (format nil "~d" timeout)
-                                                  path)
-                                            :search t
-                                            :input nil
-                                            :output paradox-out
-                                            :error paradox-err
-                                            :wait t))
-              (paradox-exit-code (process-exit-code paradox-process)))
-         (unwind-protect
-	      (if (zerop paradox-exit-code)
-		  (make-instance 'paradox-result
-				 :text (get-output-stream-string paradox-out))
-		  (make-instance 'paradox-result
-				 :text ""
-				 :szs-status (lookup-szs-status "Error")))
-	   (close paradox-out)
-	   (close paradox-err)))))))
+     (let* ((path (native-namestring (path problem)))
+	    (paradox-out (make-string-output-stream))
+	    (paradox-err (make-string-output-stream))
+	    (paradox-process (run-program "paradox"
+					  (list "--tstp"
+						"--no-progress"
+						"--time" (format nil "~d" timeout)
+						path)
+					  :search t
+					  :input nil
+					  :output paradox-out
+					  :error paradox-err
+					  :wait t))
+	    (paradox-exit-code (process-exit-code paradox-process)))
+       (unwind-protect
+	    (if (zerop paradox-exit-code)
+		(make-instance 'paradox-result
+			       :text (get-output-stream-string paradox-out))
+		(make-instance 'paradox-result
+			       :text ""
+			       :szs-status (lookup-szs-status "Error")))
+	 (close paradox-out)
+	 (close paradox-err))))))
 
 (defmethod solve :before (prover problem &key timeout)
   (declare (ignore prover problem))
