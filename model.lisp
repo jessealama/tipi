f435bac9edfc30544c3a51f882cf99bcc0a048cb
Jesse Alama <jesse.alama@gmail.com>
2013-03-04 16:21:35 +0100

    Use an auxiliary method to deal with the conjecture case.

--- model.lisp
+++ model.lisp
@@ -7,14 +7,16 @@
 (defmethod satisfiable-p ((path pathname) &key timeout)
   (satisfiable-p (parse-tptp path) :timeout timeout))
 
+(defmethod satisfiable-p :around ((db tptp-db) &key timeout)
+  (if (has-conjecture-p db)
+      (satisfiable-p (promote-conjecture-to-axiom db) :timeout timeout)
+      (call-next-method)))
+
 (defmethod satisfiable-p ((db tptp-db) &key timeout)
   (setf timeout (or timeout +default-timeout+))
-  (let ((conjecture (conjecture-formula db)))
-    (if conjecture
-	(satisfiable-p (promote-conjecture-to-axiom db) :timeout timeout)
-	(let ((szs (solve-problem db :timeout timeout)))
-	  (values (szs-implies? szs (lookup-szs-status "Satisfiable"))
-		  szs)))))
+  (let ((szs (solve-problem db :timeout timeout)))
+    (values (szs-implies? szs (lookup-szs-status "Satisfiable"))
+	    szs)))
 
 (defun consistent-premises? (problem &optional (solver *paradox*))
   (satisfiable? (remove-conjecture problem) solver))
